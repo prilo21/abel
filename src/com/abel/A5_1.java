bb44d16768edf408280d96d8c83842631c89adc6
abel <1343419761@qq.com>
2017-10-21 20:48:28 +0800

    修改

--- src/com/abel/A5_1.java
+++ src/com/abel/A5_1.java
@@ -7,13 +7,42 @@ package com.abel;
 public class A5_1 {
 
 	
-	public int getMoney() {
+	public double getMoney(String a) {
 		//写方法
-		return 0;
+		double price = 100;
+		if(a.equals("学生证")){
+			price = price*0.5;
+		}
+		if(a.equals("军官证")){
+			price = 0;
+		}
+		if(a.equals("残疾人证")){
+			price = 0;
+		}
+		if(a.length()==18){
+			String b = a.substring(6, 10);
+			int c = Integer.parseInt(b);
+			int f = 2017- c;
+			 if(0<f && f<=10){
+				 return 0;
+			 }
+			 if(11<f && 20>=f){
+				 return price*3/4;
+			 }
+			 if(20<f && 60>=f){
+				 return price;
+			 }
+			 if(f>60){
+				 return price/2;
+			 }
+		}
+		return price;
 	}
 	
 	//测试类
 	public static void main(String[] args) {
-		//System.out.println("372929199104012769");
+		A5_1  c = new   A5_1();
+		System.out.println(c.getMoney("学生证"));
+		System.out.println(c.getMoney("372929199104012769"));
 	}
 }
--- src/com/abel/A6.java
+++ src/com/abel/A6.java
@@ -11,13 +11,50 @@ package com.abel;
  */
 public class A6 {
 
-	public int getMoney(int money, int year) {
+	public double getMoney(double money, int year) {
+		double  price = 0;
 		//Ð´·½·¨
-		return 0;
+		if(money>1 && money<=100000){
+			if(year==1){
+				price  = money*0.02 + money;
+			}
+			if(year==2){
+				price = money*0.06 + money;
+			}
+			if(year==3){
+				price = money*0.12 + money;
+			}
+		}
+		
+		if(money>300000 && money<=1000000){
+			if(year==1){
+				price = money*0.04 +money;
+			}
+			if(year==2){
+				price  = money*0.08 + money;
+			}
+			if(year==3){
+				price  = money*0.15 + money;
+			}
+		}
+		
+		if(money>100000 && money<=300000){
+			if(year==1){
+				price = money*0.03 +money;
+			}
+			if(year==2){
+				price  = money*0.10 + money;
+			}
+			if(year==3){
+				price  = money*0.18 + money;
+			}
+		}
+		System.out.println(price);
+		return price;
 	}
 	
 	public static void main(String[] args) {
 		A6 a = new A6();
-		//System.out.println(a.getMoney(100000,2));
+		a.getMoney(100000,2);
 	}
 }
